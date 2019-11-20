5de076ec1bc3bb4c4a67d96525e4d316f1ba65e3
tomoya <17612720824@163.com>
2019-11-20 18:43:57 +0800

    tomoya

---
+++ json/json_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 00:48:10 2019
+
+@author: tomoya
+"""
+
+import json
+fs=open('1.json','r')
+dict1=json.load(fs)
+print(dict1)
